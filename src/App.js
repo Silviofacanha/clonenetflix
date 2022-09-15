d6c006f79504ddedd12b11b86cda6e438aea8a72
Silviofacanha <silvio.facanha@gmail.com>
2022-09-15 17:09:05 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import FeaturedMovie from './components/FeaturedMovie'
 import Header from './components/Header'
 import MovieRow from './components/MovieRow'
 
-export default () => {
-  const [blackHeader, setBlackHeader] = useState(false)
+export default () => {  
+
   const [movieList, setMovieList] = useState ([])
   const [featuredData, setFeaturedData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() => {
     const loadAll = async  () => {
-      let list = await Tmdb.getHomeList()
-      setMovieList(list)
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
       let originals = list.filter(i => i.slug === 'originals')
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen]
@@ -47,14 +48,14 @@ export default () => {
 <Header black={blackHeader} />
 
 {featuredData &&
-<FeaturedMovie item={featuredData}/>
+<FeaturedMovie item={featuredData} />
 }
 
-     <div className="Lists">
+     <section className="Lists">
         {movieList.map((item, key) => (
         <MovieRow key={key} title={item.title} items={item.items} />
       ))}
-        </div>
+        </section>
     
     <footer>
       Feito por Silvio Façanha <br />
